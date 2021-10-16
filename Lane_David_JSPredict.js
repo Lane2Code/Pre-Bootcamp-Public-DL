61855b7c5957652a4d29969e7a05c1d21d2f80a4
Lane2Code <92408522+Lane2Code@users.noreply.github.com>
2021-10-16 12:10:32 -0400

    Add files via upload

---
+++ Lane_David_JSPredict.js
@@ -0,0 +1,12 @@
+<h1>Intro to Javascript Code Predict Challenge</h1>
+<h3>David Lane</h3>
+<h6>~~~~
+
+</h6>
+//PREDICT 1 -- Console.log would log: "I was born in 1980"
+//PREDICT 2 -- Console would log "I was born in 1980"
+
+//PREDICT 3 -- Console would log "Summing Numbers"
+    //Num1 is 10
+    //Num2 is 20
+    //30
