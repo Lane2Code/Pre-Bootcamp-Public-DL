6b7c85cd403491de0003d7b4a607138687c7a2e6
Lane2Code <92408522+Lane2Code@users.noreply.github.com>
2021-10-24 14:46:52 -0400

    Add files via upload

---
+++ Lane_David_JS_tinkering.js
@@ -0,0 +1,10 @@
+
+for(var childHeight=36;childHeight<55;childHeight++)
+function (displayIfChildIsAbleToRideTheRollerCoaster) {
+    
+    childHeight = childHeight+6
+    if childHeight<52 console.log("Sorry, kiddo. Maybe next year.")
+
+    else console.log("Get on that ride, kiddo!")
+}
+console.log(childHeight)
